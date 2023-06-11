3e52efbffea82dc1f370b7920875c1418f2b8f7c
XBan <1205254918@qq.com>
2023-06-11 16:54:41 +0800

    chores: 完成feed流接口部分mock（主要是测试dao），增加并发测试

--- video-service/apps/video/impl/video_test.go
+++ video-service/apps/video/impl/video_test.go
@@ -37,35 +37,57 @@ func TestPublishVideo(t *testing.T) {
 	}
 }
 
+
 func TestFeedVideos(t *testing.T) {
+	// ? 暂缺带 token 的测试，好像也不清楚具体要测试的地方
+
+	// t.Parallel()
+	// 不确定是否需要和其他函数并发测试
+
 	// ------------- 测试准备 -----------------
-
 	should := assert.New(t)
-
 	// 创建一个真实的 GIN context
 	ctx, _ := gin.CreateTestContext(httptest.NewRecorder())
-
 	// 设置请求参数
 	request := video.NewFeedVideosRequest()
 	// 将请求参数绑定到 GIN context
 	ctx.Set("request", request)
-
 	// 调用被测试的函数
 	set, err := service.FeedVideos(ctx, request)
 
-	// ------------- 简单调用测试 -----------------
-
-
+	// ------------- 调用测试 -----------------
+	// 第一次调用
 	if should.NoError(err, "不出现错误") && should.NotNil(set, "不返回空响应") {
-		t.Log("VideoList:", set.VideoList)
-		t.Log("NextTime:", set.NextTime)
+		t.Logf("VideoList: %v\n", set.VideoList)
+		t.Logf("NextTime: %v\n", set.NextTime)
 	} else if err != nil {
 		// 错误处理逻辑
-		t.Errorf("FeedVideos returned an error: %v", err)
+		t.Errorf("FeedVideos returned an error: %v\n", err)
 	}
 
-	// 有待添加具体错误类型的判断
+	// 第二次，将上一次的 NextTime 作为参数，创建新的请求
+	// 这里 request 和 set 都得用新的变量，不然前者会请求不了，
+	// 后者会导致第一次输出也和第二次一样
+	request2 := video.NewFeedVideosRequest()
+	request2.LatestTime = set.NextTime
+	set2, err2 := service.FeedVideos(ctx, request2)
+	t.Log(set)
+	if should.NoError(err2, "不出现错误") && should.NotNil(set2, "不返回空响应") {
+		t.Log("\n第二次请求")
+		t.Logf("VideoList: %v\n", set.VideoList)
+		t.Logf("NextTime: %v\n", set.NextTime)
+	} else if err2 != nil {
+		// 错误处理逻辑
+		t.Errorf("FeedVideos returned an error: %v\n", err2)
+	}
 
+	// 这里没有想到好办法可以判断查到第二次的视频是不是正确的
+	// （按照时间接着上一次的），就暂时还是只能判断是否出错
+	// 以及具体的错误感觉也不需要判断
+
+}
+
+func TestFeedVideosMultiThreads(t *testing.T) {
 	// ------------- 并发查询测试 -----------------
 	var wg sync.WaitGroup
 	const numWorkers = 10
@@ -74,19 +96,14 @@ func TestFeedVideos(t *testing.T) {
 	for i := 0; i < numWorkers; i++ {
 		go func() {
 			defer wg.Done()
-
-			request := video.NewFeedVideosRequest()
-			set, err := service.FeedVideos(context.Background(), request)
-
-			if should.NoError(err, "不出现错误") && should.NotNil(set, "不返回空响应") {
-				t.Log("VideoList:", set.VideoList)
-				t.Log("NextTime:", set.NextTime)
-			}
+			// 往下填充需要并发测试的部分
+			t.Run("FeedVideos", TestFeedVideos)
 		}()
 	}
 
 	wg.Wait()
 }
+
 
 func TestPublishList(t *testing.T) {
 	// ------------- 测试准备 -----------------
@@ -118,28 +135,27 @@ func TestPublishList(t *testing.T) {
 	// 有待添加具体错误类型的判断
 
 	// ------------- 并发查询测试 -----------------
-	var wg sync.WaitGroup
-	const numWorkers = 10
-	wg.Add(numWorkers)
+	// var wg sync.WaitGroup
+	// const numWorkers = 10
+	// wg.Add(numWorkers)
 
-	for i := 0; i < numWorkers; i++ {
-		go func() {
-			defer wg.Done()
+	// for i := 0; i < numWorkers; i++ {
+	// 	go func() {
+	// 		defer wg.Done()
 
-			request := video.NewPublishListRequest()
-			request.UserId = 17
+	// 		request := video.NewPublishListRequest()
+	// 		request.UserId = 17
 
-			set, err := service.PublishList(context.Background(), request)
+	// 		set, err := service.PublishList(context.Background(), request)
 
-			if should.NoError(err, "不出现错误") && should.NotNil(set, "不返回空响应") {
-				t.Log("VideoList:", set.VideoList)
-			}
-		}()
-	}
+	// 		if should.NoError(err, "不出现错误") && should.NotNil(set, "不返回空响应") {
+	// 			t.Log("VideoList:", set.VideoList)
+	// 		}
+	// 	}()
+	// }
 
-	wg.Wait()
+	// wg.Wait()
 }
-
 
 func TestGetVideo(t *testing.T) {
 	should := assert.New(t)
